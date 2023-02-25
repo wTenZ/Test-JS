20a90cf33194c520da44971df779b611aa7fffac
Tatsu <wsololvl@gmail.com>
2023-02-25 08:00:55 -0500

    writing HTML from JS

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelectorAll('p');
-const pclass = document.querySelector('.pclass');
-const pid = document.querySelector('#pid');
+const pclass = document.getElementsByClassName('.pclass');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log(h1)
@@ -13,3 +13,16 @@ console.log({
     pid,
     input
 });
+
+h1.innerHTML = 'wwwww <br> Devil Bluess';
+h1.innerText = 'wwwww <br> Devil Bluess';
+//console.log(h1.getAttribute('class'))
+//h1.setAttribute('class', 'newValue')
+h1.classList.add('newValue');
+h1.classList.remove('showmaker');
+//h1.classList.toggle('showmaker')
+input.value = '1234';
+const img = document.createElement('img');
+img.setAttribute('src', 'http://www.deculture.es/wp-content/uploads/2014/11/i-am-a-hero.jpg');
+pid.innerText = ""
+pid.appendChild(img);
