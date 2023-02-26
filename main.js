700dec71f3db95e91fcda8e9c68e37d522cabbbb
Tatsu <wsololvl@gmail.com>
2023-02-26 08:02:30 -0500

    JS events

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelectorAll('p');
-const pclass = document.getElementsByClassName('.pclass');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#n1');
+const input2 = document.querySelector('#n2');
+const OP = document.querySelector('#OP');
+const res = document.querySelector('#resultado')
 
-console.log(h1)
-console.log(input.value)
-console.log({
-    h1,
-    p,
-    pclass,
-    pid,
-    input
-});
+function btnOP(){
+    x = Number(input1.value);
+    y = Number(input2.value);
 
-h1.innerHTML = 'wwwww <br> Devil Bluess';
-h1.innerText = 'wwwww <br> Devil Bluess';
-//console.log(h1.getAttribute('class'))
-//h1.setAttribute('class', 'newValue')
-h1.classList.add('newValue');
-h1.classList.remove('showmaker');
-//h1.classList.toggle('showmaker')
-input.value = '1234';
-const img = document.createElement('img');
-img.setAttribute('src', 'http://www.deculture.es/wp-content/uploads/2014/11/i-am-a-hero.jpg');
-pid.innerText = ""
-pid.appendChild(img);
+    w = x + y;
+    res.innerHTML = `${x} + ${y} = ${w}`;
+
+}
