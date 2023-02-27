9563fe28f7ae7a47f2d7009d218d9ee94f097bef
Tatsu <wsololvl@gmail.com>
2023-02-27 05:48:48 -0500

    addEventListener

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#formulario');
 const input1 = document.querySelector('#n1');
 const input2 = document.querySelector('#n2');
 const OP = document.querySelector('#OP');
 const res = document.querySelector('#resultado')
 
-function btnOP(){
+
+form.addEventListener('click',sumOP)
+
+function sumOP(){
     x = Number(input1.value);
     y = Number(input2.value);
-
+    //event.preventDefault();
     w = x + y;
     res.innerHTML = `${x} + ${y} = ${w}`;
 
